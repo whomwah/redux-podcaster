9394693c0d2e91acb6da18ed506ebfc7ba67935f
Duncan Robertson <duncan@whomwah.com>
2009-04-03 21:40:50 +0100

    recent sort now more efficient

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,7 +2,8 @@ module HomeHelper
 
   def recent_subscriptions
     subs = []
-    Dir.glob(File.join(RAILS_ROOT, "tmp/cache", "**", "*.xml.cache")).each do |fn|
+    sorted = Dir.glob(File.join(RAILS_ROOT, "tmp/cache", "**", "*.xml.cache")).sort_by {|f| test(?M, f)}
+    sorted.each do |fn|
       subs << File.open(fn, 'rb') { |f| Marshal.load(f) } rescue []
     end
     subs.reverse[0...5]
