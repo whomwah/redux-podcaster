d6c26b499ffdb4b3fafa66e8f52eada9af5b5d55
Duncan Robertson <duncan@whomwah.com>
2009-04-03 16:58:03 +0100

    homepage now has recent subscriptions

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,12 @@
 module HomeHelper
+
+  def recent_subscriptions
+    subs = []
+    Dir.glob(File.join(RAILS_ROOT, "tmp/cache", "**", "*.xml.cache")).each do |fn|
+      subs << File.open(fn, 'rb') { |f| Marshal.load(f) } rescue []
+      break if subs.size >= 5
+    end
+    subs.reverse
+  end
+
 end
